494589b0b7bd3093e8601f82d858e148169f99c4
Hastarus <Hugo.ustariz@gmail.com>
2024-04-26 16:23:49 +0200

    Part 1 React Courses
    Course is over, now exercises!

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -7,17 +7,35 @@ const Hello = (props) => {
   )
 }
 
+const Footer = () => {
+  return (
+    <div>
+      Greeting app created by <a href='https://github.com/hustariz'>hastarus</a>
+    </div>
+  )
+}
+
 const App = () => {
   const name = 'Peter'
   const age = 10
+  const friends = [
+    { name: 'Adrien', age: 30},
+    { name: 'Lucie', age: 25},
+  ]
+  const friendsarray = ['friendfromarray1 ', 'friendfromarray2']
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
 
       <Hello />
       <Hello name='George' age={26+10}/>
       <Hello name={name} age={age}/>
-    </div>
+      <p>my friends are:</p>
+      <p>{friends[0].name}, {friends[0].age} ans.</p>
+      <p>{friends[1].name}, {friends[1].age} ans.</p>
+      <p>{friendsarray}</p>
+      <Footer />
+    </>
   )
 }
 
