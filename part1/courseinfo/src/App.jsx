8518fc4fb732240d6bb03a6b73cac893b65e8e5d
Hastarus <Hugo.ustariz@gmail.com>
2024-05-01 15:24:11 +0200

    Part 1 Javascript ; Ex: 1.5 DONE
    Exercice done.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -70,7 +70,9 @@ const App = () => {
 
   return (
     <div>
-      ...
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
