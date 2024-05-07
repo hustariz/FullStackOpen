5d40b7c84a51884e469d9ce3988b39313ee871f6
Hastarus <Hugo.ustariz@gmail.com>
2024-05-07 18:01:27 +0200

    Part 1.D Event handling revisited

--- part1/Courses/src/App.jsx
+++ part1/Courses/src/App.jsx
@@ -8,51 +8,15 @@ const Footer = () => {
   )
 }
 
-const Display = ({counter}) => <div>{counter}</div>
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-  
-const History = (props) => {
-  if (props.allClicks.length === 0){
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      Button press history : {props.allClicks.join(' ')}
-      <p>total {props.total}</p>
-    </div>
-  )
-}
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const[allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right)
-  }
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setTotal(left + updatedRight)
-  }
-
+  const handleResetClick = () => {setValue(0)}
 
   return (
     <div>
-      {left}
-      <Button onClick={handleLeftClick} text='left'/>
-      <Button onClick={handleRightClick} text= 'right'/>
-      {right}
-      <History allClicks={allClicks} total={total}/>
+      <p>{value}</p>
+      <button onClick={handleResetClick}>reset to zero</button>
       <Footer />
     </div>
   )
