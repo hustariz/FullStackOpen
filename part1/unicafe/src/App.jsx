391ee284b7391ffa6773f7a863293629dd31413f
Hastarus <Hugo.ustariz@gmail.com>
2024-05-10 00:59:33 +0200

    Part 1.D; Ex 1.6: DONE

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -29,10 +29,16 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const incrementGood = (newValue) => () => {setGood(newValue)}
+  const incrementNeutral = (newValue) => () => {setNeutral(newValue)}
+  const incrementBad = (newValue) => () => {setBad(newValue)}
 
   return (
     <div>
       <Title title={titleFeed}/>
+      <Button handleClick={incrementGood(good + 1)} text="Good"/>
+      <Button handleClick={incrementNeutral(neutral + 1)} text="Neutral"/>
+      <Button handleClick={incrementBad(bad + 1)} text="Bad"/>
 
       <Title title={titleStat}/>
       <Display title='Good: ' value={good}/>
