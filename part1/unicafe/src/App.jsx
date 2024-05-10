988d6d70c6fc1ba8988716a8fef1c5a1fc5a586b
Hastarus <Hugo.ustariz@gmail.com>
2024-05-10 02:59:48 +0200

    Part 1.D; Ex 1.7:  Done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,8 +12,6 @@ const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-
-
 const Footer = () => {
   return (
     <div>
@@ -22,6 +20,7 @@ const Footer = () => {
   )
 }
 
+
 const App = () => {
   // save clicks of each button to its own state
   const titleFeed = 'Give feedback'
@@ -29,21 +28,40 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const incrementGood = (newValue) => () => {setGood(newValue)}
-  const incrementNeutral = (newValue) => () => {setNeutral(newValue)}
-  const incrementBad = (newValue) => () => {setBad(newValue)}
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good+1);setAll(all+1);
+    setAverage(((good*1+neutral*0-bad)+1)/(all+1));
+    setPositive((good/(all+1))*100 + ' %');}
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral+1); setAll(all+1);
+    setAverage((good*1+neutral*0-bad)/(all+1));
+    setPositive((good/(all+1))*100 + ' %');}
+
+  const handleBadClick = () => {
+    setBad(bad+1); setAll(all+1);
+    setAverage(((good*1+neutral*0-bad)-1)/(all+1));
+    setPositive((good/(all+1))*100 + ' %');}
+    
+  
   return (
     <div>
       <Title title={titleFeed}/>
-      <Button handleClick={incrementGood(good + 1)} text="Good"/>
-      <Button handleClick={incrementNeutral(neutral + 1)} text="Neutral"/>
-      <Button handleClick={incrementBad(bad + 1)} text="Bad"/>
+      <Button handleClick={handleGoodClick} text="Good"/>
+      <Button handleClick={handleNeutralClick} text="Neutral"/>
+      <Button handleClick={handleBadClick} text="Bad"/>
 
       <Title title={titleStat}/>
       <Display title='Good: ' value={good}/>
       <Display title='Neutral: 'value={neutral}/>
       <Display title='Bad: 'value={bad}/>
+      <Display title='All: 'value={all}/>
+      <Display title='Average: 'value={average}/>
+      <Display title='Positive: 'value={positive}/>
       <br></br>
       <Footer />
     </div>
