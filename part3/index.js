665a89ace52eaa6c510a91523728445115a7a62f
Hastarus <Hugo.ustariz@gmail.com>
2024-06-12 20:49:43 +0200

    part 3.a: VSC REST client, receiving data

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
       id: 1,
@@ -37,11 +39,36 @@ let notes = [
     }
   })
 
+  const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: Boolean(body.important) || false,
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+
+    response.json(note)
+  })
+  
+
   app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    console.log(id);
     notes = notes.filter(note => note.id !== id)
-    console.log(notes);
     response.status(204).end()
 
   })
