44690e54f8dc1194974aeb7e926219ebf4797aad
Hastarus <Hugo.ustariz@gmail.com>
2024-06-17 23:55:29 +0200

    part 3.a; ex 3.8*: show data sent in HTTP POST with morgan

--- part3/Exercices/index.js
+++ part3/Exercices/index.js
@@ -5,7 +5,10 @@ const app = express()
 
 app.use(express.json())
 
-app.use(morgan('tiny'))
+morgan.token('body', (req) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 
 let contacts = [
     { 
