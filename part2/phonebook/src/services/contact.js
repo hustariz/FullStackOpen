32497e4d5aafe00b554b70c8f2df78b945fae78b
Hastarus <Hugo.ustariz@gmail.com>
2024-06-23 20:38:42 +0200

    part 3.b; ex 3.10: backend deployed and frontend linked to backend

--- part2/phonebook/src/services/contact.js
+++ part2/phonebook/src/services/contact.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3002/api/contacts'
+const baseUrl = 'https://fullstackopen-exercices.onrender.com/api/contacts'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
