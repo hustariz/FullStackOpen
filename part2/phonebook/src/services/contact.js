335a7079ed57dc58ec3624207bd1a06f73310fa6
Hastarus <Hugo.ustariz@gmail.com>
2024-06-24 16:12:03 +0200

    part 3.b; ex 3.11: the whole app to internet

--- part2/phonebook/src/services/contact.js
+++ part2/phonebook/src/services/contact.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://fullstackopen-exercices.onrender.com/api/contacts'
+const baseUrl = '/api/contacts'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
